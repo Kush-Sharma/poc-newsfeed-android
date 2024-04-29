e3f169dfca5691da5b8ae595b4e40623d39db0df
Kush Sharma <shubhendu.sh@gmail.com>
2024-04-29 13:06:17 +1200

    Update System bar colors

--- app/src/main/java/com/example/newsfeedapp/MainActivity.kt
+++ app/src/main/java/com/example/newsfeedapp/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
@@ -18,6 +21,7 @@ import com.example.newsfeedapp.presentation.navgraph.NavGraph
 import com.example.newsfeedapp.presentation.onboarding.OnBoardingScreen
 import com.example.newsfeedapp.presentation.onboarding.OnBoardingViewModel
 import com.example.newsfeedapp.ui.theme.NewsFeedAppTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,6 +38,16 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             NewsFeedAppTheme {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
